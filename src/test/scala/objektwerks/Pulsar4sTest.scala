0be7647041f13a57bb2486f1d16e7259e36a0627
objektwerks <objektwerks@runbox.com>
2022-05-12 08:01:57 -0400

    implemented pulsar4s test; edited test.conf

--- src/test/scala/objektwerks/Pulsar4sTest.scala
+++ src/test/scala/objektwerks/Pulsar4sTest.scala
@@ -1,42 +1,40 @@
 package objektwerks
 
-import com.sksamuel.pulsar4s.{DefaultProducerMessage, EventTime, ProducerConfig, PulsarClient, Topic}
+import com.sksamuel.pulsar4s._
 import com.sksamuel.pulsar4s.circe._
 import com.typesafe.config.ConfigFactory
 
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.must.Matchers
-
 import io.circe.generic.auto._
 
-class Pulsar4sTest extends AnyFunSuite with Matchers {
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util.{Failure, Success}
+
+case class Event(key: Int, value: Int)
+
+class Pulsar4sTest extends AnyFunSuite {
   val conf = ConfigFactory.load("test.conf")
   val url = conf.getString("url")
   val topicName = conf.getString("topicName")
   val producerName = conf.getString("producerName")
-
-  val client = PulsarClient(url)
-  val topic = Topic(topicName)
-
-  val producer = client.producer[SensorEvent](ProducerConfig(
-    topic, 
-    producerName = Some(producerName),
-    enableBatching = Some(true),
-    blockIfQueueFull = Some(true))
-  )
-
-
+  val subscriptionName = conf.getString("subscriptionName")
 
   test("produce > consume") {
+    val client = PulsarClient(url)
+    val topic = Topic(topicName)
 
-    (0 until 100) foreach { _ =>
-      val sensorEvent = SensorDomain.generate()
-      val message = DefaultProducerMessage(
-        Some(sensorEvent.sensorId), 
-        sensorEvent, 
-        eventTime = Some(EventTime(sensorEvent.eventTime)))
-      
-      producer.sendAsync(message) 
+    val producer = client.producer[Event](ProducerConfig(topic))
+    producer.send( Event(1, 1) )
+
+    val subscription = Subscription(subscriptionName)
+    val consumerConfig = ConsumerConfig(subscription, Seq(topic))
+    val consumer = client.consumer[Event](consumerConfig)
+    consumer.receive match {
+      case Success(message) => assert( message.value.key == message.value.value )
+      case Failure(error) => fail(error.getMessage())
     }
+
+    producer.close()
+    consumer.close()
   }
 }
